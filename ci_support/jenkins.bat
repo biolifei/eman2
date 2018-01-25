d0d452484f1646e1f3024c183b9c8ceba4b8b6fc
shadow_walker <shadowwalkersb@gmail.com>
2018-01-25 16:29:33 -0600

    if errorlevel 1 exit 1 [ci skip]

--- ci_support/jenkins.bat
+++ ci_support/jenkins.bat
@@ -2,3 +2,4 @@ set BASH_EXE=C:\Users\EMAN\Miniconda2\Library\bin\bash.exe
 set "BASH_CMD=%BASH_EXE% --login -i -c"
 
 %BASH_CMD% "bash C:/Users/EMAN/workspace/build-scripts-cron/cronjob.sh win"
+if errorlevel 1 exit 1
