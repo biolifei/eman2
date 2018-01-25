0dde1854d4cdbbfb1556086de9180d2cdca39e34
shadow_walker <shadowwalkersb@gmail.com>
2018-01-25 16:23:07 -0600

    BASH_EXE [ci skip]

--- ci_support/jenkins.bat
+++ ci_support/jenkins.bat
@@ -1,1 +1,3 @@
-C:\Users\EMAN\Miniconda2\Library\bin\bash.exe --login -i -c "bash C:/Users/EMAN/workspace/build-scripts-cron/cronjob.sh win"
+set BASH_EXE=C:\Users\EMAN\Miniconda2\Library\bin\bash.exe
+
+%BASH_EXE% --login -i -c "bash C:/Users/EMAN/workspace/build-scripts-cron/cronjob.sh win"
