7b3a70ea66b66e9438a915a7c5b3fa12a1907ce0
shadow_walker <shadowwalkersb@gmail.com>
2018-01-25 19:56:11 -0600

    release/windows [ci skip]

--- ci_support/jenkins.bat
+++ ci_support/jenkins.bat
@@ -4,7 +4,7 @@ set INSTALLER_DIR=C:\Users\EMAN\workspace\win-installers
 set INSTALLER_FILE=eman2.21.win64.exe
 set INSTALLATION_DIR=eman2-21-win64
 
-%BASH_CMD% "bash C:/Users/EMAN/workspace/build-scripts-cron/cronjob.sh win"
+%BASH_CMD% "bash C:/Users/EMAN/workspace/build-scripts-cron/cronjob.sh win release/windows"
 if errorlevel 1 exit 1
 
 rmdir /q /s %INSTALLER_DIR%\%INSTALLATION_DIR%
