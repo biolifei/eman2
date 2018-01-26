1c825418ea55d74a99e21b9a0ec1e03e8c16a152
shadow_walker <shadowwalkersb@gmail.com>
2018-01-25 22:41:15 -0600

    git-bash [ci skip]

--- ci_support/jenkins.bat
+++ ci_support/jenkins.bat
@@ -1,5 +1,5 @@
-set BASH_EXE=C:\Users\EMAN\Miniconda2\Library\bin\bash.exe
-set "BASH_CMD=%BASH_EXE% --login -i -c"
+set "BASH_EXE=C:\Program Files\Git\bin\bash.exe"
+set "BASH_CMD=%BASH_EXE% -c"
 set INSTALLER_DIR=C:\Users\EMAN\workspace\win-installers
 set INSTALLER_FILE=eman2.21.win64.exe
 set INSTALLATION_DIR=eman2-21-win64
