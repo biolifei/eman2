2316774da3973507fbdcd272243848022c2d42b1
shadow_walker <shadowwalkersb@gmail.com>
2018-01-25 19:47:43 -0600

    set vars [ci skip]

--- ci_support/jenkins.bat
+++ ci_support/jenkins.bat
@@ -1,5 +1,8 @@
 set BASH_EXE=C:\Users\EMAN\Miniconda2\Library\bin\bash.exe
 set "BASH_CMD=%BASH_EXE% --login -i -c"
+set INSTALLER_DIR=C:\Users\EMAN\workspace\win-installers
+set INSTALLER_FILE=eman2.21.win64.exe
+set INSTALLATION_DIR=eman2-21-win64
 
 %BASH_CMD% "bash C:/Users/EMAN/workspace/build-scripts-cron/cronjob.sh win"
 if errorlevel 1 exit 1
